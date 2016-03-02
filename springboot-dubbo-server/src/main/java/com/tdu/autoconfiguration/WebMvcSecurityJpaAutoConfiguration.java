1d2ae28f216539c515983ef735c6d137a0c7050c
tangdu <tangdu0228yes@163.com>
2016-03-02 22:43:51 +0800

    新增httpclient测试接口用例

--- springboot-dubbo-server/src/main/java/com/tdu/autoconfiguration/WebMvcSecurityJpaAutoConfiguration.java
+++ springboot-dubbo-server/src/main/java/com/tdu/autoconfiguration/WebMvcSecurityJpaAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.tdu.autoconfiguration;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -8,10 +9,18 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.Headers;
+import org.springframework.boot.autoconfigure.security.SpringBootWebSecurityConfiguration;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -20,38 +29,117 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.header.writers.HstsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 @Configuration
-@AutoConfigureBefore(SecurityAutoConfiguration.class)
+@AutoConfigureBefore(SpringBootWebSecurityConfiguration.class)
+@EnableConfigurationProperties
+@ConditionalOnClass({ EnableWebSecurity.class, AuthenticationEntryPoint.class })
+@ConditionalOnMissingBean(WebSecurityConfiguration.class)
+@ConditionalOnWebApplication
 @EnableWebSecurity
 public class WebMvcSecurityJpaAutoConfiguration {
-	@Bean
-	@ConditionalOnMissingBean
-	public PasswordEncoder passwordEncoder() {
-		PasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder;
-	}
-
-	@Bean
-	@ConditionalOnMissingBean
-	public WebSecurityConfigurer<WebSecurity> webMvcSecurityConfiguration() {
-		return new WebMvcSecurityConfiguration();
-	}
-
 	@Bean
 	@ConditionalOnMissingBean
 	public AuthenticationManager webMvcSecurityAuthenticationManager(AuthenticationConfiguration auth)
 			throws Exception {
 		return auth.getAuthenticationManager();
 	}
+	
+	private static List<String> DEFAULT_IGNORED = Arrays.asList("/css/**", "/js/**", "/images/**", "/**/favicon.ico",
+			"/download/**", "/webjars/**", "/resources/**");
+	
+	@Bean
+	@ConditionalOnMissingBean({ IgnoredPathsWebSecurityConfigurerAdapter.class })
+	public IgnoredPathsWebSecurityConfigurerAdapter ignoredPathsWebSecurityConfigurerAdapter() {
+		return new IgnoredPathsWebSecurityConfigurerAdapter();
+	}
 
-	private static class WebMvcSecurityConfiguration extends WebSecurityConfigurerAdapter {
+	public static void configureHeaders(HeadersConfigurer<?> configurer, SecurityProperties.Headers headers)
+			throws Exception {
+		if (headers.getHsts() != Headers.HSTS.NONE) {
+			boolean includeSubdomains = headers.getHsts() == Headers.HSTS.ALL;
+			HstsHeaderWriter writer = new HstsHeaderWriter(includeSubdomains);
+			writer.setRequestMatcher(AnyRequestMatcher.INSTANCE);
+			configurer.addHeaderWriter(writer);
+		}
+		if (!headers.isContentType()) {
+			configurer.contentTypeOptions().disable();
+		}
+		if (!headers.isXss()) {
+			configurer.xssProtection().disable();
+		}
+		if (!headers.isCache()) {
+			configurer.cacheControl().disable();
+		}
+		if (!headers.isFrame()) {
+			configurer.frameOptions().disable();
+		}
+	}
+
+	public static List<String> getIgnored(SecurityProperties security) {
+		List<String> ignored = new ArrayList<String>(security.getIgnored());
+		ignored.addAll(DEFAULT_IGNORED);//合并默认路径
+		if (ignored.contains("none")) {
+			ignored.remove("none");
+		}
+		return ignored;
+	}
+
+	@Order(SecurityProperties.IGNORED_ORDER)
+	private static class IgnoredPathsWebSecurityConfigurerAdapter implements WebSecurityConfigurer<WebSecurity> {
+
+		@Autowired(required = false)
+		private ErrorController errorController;
+
+		@Autowired
+		private SecurityProperties security;
+
+		@Autowired
+		private ServerProperties server;
+
+		@Override
+		public void configure(WebSecurity builder) throws Exception {
+		}
+
+		@Override
+		public void init(WebSecurity builder) throws Exception {
+			List<String> ignored = getIgnored(this.security);
+			if (this.errorController != null) {
+				ignored.add(normalizePath(this.errorController.getErrorPath()));
+			}
+			String[] paths = this.server.getPathsArray(ignored);
+			if (!ObjectUtils.isEmpty(paths)) {
+				builder.ignoring().antMatchers(paths);
+			}
+		}
+
+		private String normalizePath(String errorPath) {
+			String result = StringUtils.cleanPath(errorPath);
+			if (!result.startsWith("/")) {
+				result = "/" + result;
+			}
+			return result;
+		}
+	}
+	
+	@Bean
+	@ConditionalOnMissingBean
+	public WebMvcSecurityConfiguration WebMvcSecurityConfiguration(AuthenticationConfiguration auth)
+			throws Exception {
+		return new WebMvcSecurityConfiguration();
+	}
+
+	protected static class WebMvcSecurityConfiguration extends WebSecurityConfigurerAdapter {
 		@Autowired
 		private Environment environment;
 
@@ -59,7 +147,7 @@ public class WebMvcSecurityJpaAutoConfiguration {
 		protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 			auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
 		}
-
+		
 		@Override
 		protected void configure(HttpSecurity http) throws Exception {
 			Boolean eanbleCsrf = environment.getProperty("security.enable-csrf", Boolean.class, false);
@@ -75,8 +163,15 @@ public class WebMvcSecurityJpaAutoConfiguration {
 
 			http.authorizeRequests().antMatchers(getPermitAllUrlAntMatchers()).permitAll().anyRequest().authenticated();
 
-			http.formLogin().loginPage("/login.html").loginProcessingUrl("/login").failureUrl("/login.html").permitAll()
-					.and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
+			http.formLogin()
+				.loginPage("/login.html")
+				.loginProcessingUrl("/login")
+				.failureUrl("/login.html")
+				.permitAll()
+				.and()
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/");
 		}
 
 		private class CsrfProtectionMatcher implements RequestMatcher {
--- springboot-http-client/src/main/java/com/client/ApplicationDubboClient.java
+++ springboot-http-client/src/main/java/com/client/ApplicationDubboClient.java
@@ -4,26 +4,23 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.tdu.service.UserInfoService;
-
 @RunWith(SpringJUnit4ClassRunner.class)
+@ComponentScan(basePackages={"com.client"})
 @SpringApplicationConfiguration(classes = ApplicationDubboClient.class)
-@ImportResource({"classpath:application_custom.xml"})
 public class ApplicationDubboClient {
-
 	@Autowired
-	private UserInfoService userInfoService;
-	
+	private HttpService httpService;
+
 	@Test
-	public void run(){
+	public void run() {
 		long a=0;
-		for(int i=0;i<100000;i++){
-			long s=System.nanoTime();
-			userInfoService.hello();
-			long e=System.nanoTime();
+		for (int i = 0; i < 100000; i++) {
+			long s = System.nanoTime();
+			httpService.hello2();
+			long e = System.nanoTime();
 			a+=(e-s);
 		}
 		System.out.println("total:"+a/1000d/1000/1000);
